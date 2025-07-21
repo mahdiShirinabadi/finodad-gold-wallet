d5baf051c494c0b5926ef7c9f6c9e8c65f070146
Mahdi <admin@admin.com>
2025-07-21 13:20:00 +0330

    fix transaction call

---
+++ gold-wallet-core/src/main/java/com/melli/wallet/domain/request/limitation/CreateLimitationGeneralCustomRequestJson.java
@@ -0,0 +1,64 @@
+package com.melli.wallet.domain.request.limitation;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.melli.wallet.NamingProperty;
+import com.melli.wallet.annotation.number.NumberValidation;
+import com.melli.wallet.annotation.string.StringValidation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+/**
+ * Class Name: UpdateLimitationGeneralCustomRequest
+ * Author: Mahdi Shirinabadi
+ * Date: 1/20/2025
+ */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CreateLimitationGeneralCustomRequestJson {
+
+    @Schema(name = "limitationGeneralId")
+    @JsonProperty("limitationGeneralId")
+    @NotNull(message = "General limitation ID is required")
+    @NumberValidation(label = "limitationGeneralId")
+    private Long limitationGeneralId;
+
+    @Schema(name = NamingProperty.VALUE)
+    @JsonProperty(NamingProperty.VALUE)
+    @StringValidation(minLength = "1", label = NamingProperty.VALUE)
+    @NotBlank(message = "Limitation value is required")
+    private String value;
+
+    @Schema(name = NamingProperty.ADDITIONAL_DATA)
+    @JsonProperty(NamingProperty.ADDITIONAL_DATA)
+    private String additionalData;
+
+    @Schema(name = "walletLevelId")
+    @JsonProperty("walletLevelId")
+    @NumberValidation(label = "walletLevelId")
+    private Long walletLevelId;
+
+    @Schema(name = "walletAccountTypeId")
+    @JsonProperty("walletAccountTypeId")
+    @NumberValidation(label = "walletAccountTypeId")
+    private Long walletAccountTypeId;
+
+    @Schema(name = "walletAccountCurrencyId")
+    @JsonProperty("walletAccountCurrencyId")
+    @NumberValidation(label = "walletAccountCurrencyId")
+    private Long walletAccountCurrencyId;
+
+    @Schema(name = "walletTypeId")
+    @JsonProperty("walletTypeId")
+    @NumberValidation(label = "walletTypeId")
+    private Long walletTypeId;
+
+    @Schema(name = "channelId")
+    @JsonProperty("channelId")
+    @NumberValidation(label = "channelId")
+    private Long channelId;
+} 
---
+++ gold-wallet-core/src/main/java/com/melli/wallet/domain/request/limitation/UpdateLimitationGeneralRequestJson.java
@@ -0,0 +1,37 @@
+package com.melli.wallet.domain.request.limitation;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.melli.wallet.NamingProperty;
+import com.melli.wallet.annotation.number.NumberValidation;
+import com.melli.wallet.annotation.string.StringValidation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+/**
+ * Class Name: UpdateLimitationGeneralRequest
+ * Author: Mahdi Shirinabadi
+ * Date: 1/20/2025
+ */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UpdateLimitationGeneralRequestJson {
+
+    @Schema(name = NamingProperty.ID)
+    @JsonProperty(NamingProperty.ID)
+    @NotBlank(message = "Limitation id is required")
+    @NumberValidation
+    private String id;
+
+    @Schema(name = NamingProperty.VALUE)
+    @JsonProperty(NamingProperty.VALUE)
+    @StringValidation(minLength = "1", message = "Limitation value is required")
+    private String value;
+
+    @Schema(name = NamingProperty.PATTERN)
+    @JsonProperty(NamingProperty.PATTERN)
+    private String pattern;
+} 
