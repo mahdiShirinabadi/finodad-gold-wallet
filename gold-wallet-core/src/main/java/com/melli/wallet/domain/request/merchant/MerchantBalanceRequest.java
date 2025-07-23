6cce244faed3e29fc13dee259594a32d378bf2da
Mahdi <admin@admin.com>
2025-07-23 08:56:16 +0330

    add increase or decrease merchant balance

---
+++ gold-wallet-core/src/main/java/com/melli/wallet/domain/request/merchant/MerchantBalanceRequest.java
@@ -0,0 +1,35 @@
+package com.melli.wallet.domain.request.merchant;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.melli.wallet.NamingProperty;
+import com.melli.wallet.annotation.number.NumberValidation;
+import com.melli.wallet.annotation.string.StringValidation;
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.validation.Valid;
+
+/**
+ * Class Name: MerchantBalanceRequest
+ * Author: Mahdi Shirinabadi
+ * Date: 6/7/2025
+ */
+@Getter
+@Setter
+public class MerchantBalanceRequest {
+
+    @Valid
+    @StringValidation(label = "شماره حساب کیف پول")
+    @JsonProperty(NamingProperty.WALLET_ACCOUNT_NUMBER)
+    private String walletAccountNumber;
+
+    @Valid
+    @StringValidation(label = "مبلغ")
+    @JsonProperty(NamingProperty.WALLET_ACCOUNT_NUMBER)
+    private String amount;
+
+    @Valid
+    @NumberValidation(label = "شناسه پذیرنده")
+    @JsonProperty(NamingProperty.MERCHANT_ID)
+    private String merchantId;
+} 
